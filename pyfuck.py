739067767ec6c45e0bca9c05dc3057e5c4fc9b29
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-20 00:42:45 +0900

    fix: ファイル入出力コードを別関数に分離

--- pyfuck.py
+++ pyfuck.py
@@ -1,21 +1,34 @@
-print(""" ____        _____           _
+print(
+    """ ____        _____           _
 |  _ \ _   _|  ___|   _  ___| | __
 | |_) | | | | |_ | | | |/ __| |/ /
 |  __/| |_| |  _|| |_| | (__|   <
 |_|    \__, |_|   \__,_|\___|_|\_\\
        |___/
                                     by Satoki
-""")
+"""
+)
 
-with open(input("FileName: "), "r") as input_file, \
-                    open("output.py", "w") as output_file:
-    python_code = input_file.read()
-    
+# Pythonファイルを読み込み
+def read_file(filename: str):
+    with open(filename, "r") as f:
+        return f.read()
+
+
+# PyFuckファイルを書き込み
+def write_file(filename: str, fuck: str):
+    with open(filename, "w") as f:
+        return f.write(fuck)
+
+
+if __name__ == "__main__":
+    python_code = read_file(input("FileName: "))
+
     pyfuck = ""
     for c in python_code:
         pyfuck += "chr("
         ord_c = ord(c)
-        fuck = int('1' * len(str(ord_c)))
+        fuck = int("1" * len(str(ord_c)))
         while ord_c != 0:
             for i in range(int(ord_c / fuck)):
                 if pyfuck[-1] != "(":
@@ -25,5 +38,5 @@ with open(input("FileName: "), "r") as input_file, \
             fuck = int(fuck / 10)
         pyfuck += ")+"
 
-    output_file.write(f"exec({pyfuck[:-1]})")
+    write_file("output.py", f"exec({pyfuck[:-1]})")
     print("-> output.py")
