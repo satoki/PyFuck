a12eb0d19f8ebabdf9828f94d7229de45f4d6f05
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-20 00:52:58 +0900

    fix: PyFuck コードを生成する関数を分離

--- pyfuck.py
+++ pyfuck.py
@@ -9,18 +9,19 @@ print(
 """
 )
 
-# Pythonファイルを読み込み
+# Python ファイルを読み込み
 def read_file(filename: str):
     with open(filename, "r") as f:
         return f.read()
 
 
-# PyFuckファイルを書き込み
+# PyFuck ファイルを書き込み
 def write_file(filename: str, fuck: str):
     with open(filename, "w") as f:
         return f.write(fuck)
 
-# 数値をPyFuckに変換
+
+# 数値を PyFuck に変換
 def num_to_fuck(num: str):
     digit: int = len(num)  # num の文字数
     num = int(num)
@@ -33,14 +34,19 @@ def num_to_fuck(num: str):
         ones = ones // 10
     return "+".join(map(str, fucks))
 
-if __name__ == "__main__":
-    python_code = read_file(input("FileName: "))
 
+# PyFuck コードを生成
+def make_pyfuck(python_code: str):
     pyfuck = ""
     for c in python_code:
         pyfuck += "chr("
         pyfuck += num_to_fuck(str(ord(c)))
         pyfuck += ")+"
+    return pyfuck[:-1]
 
-    write_file("output.py", f"exec({pyfuck[:-1]})")
+
+if __name__ == "__main__":
+    python_code = read_file(input("FileName: "))
+    pyfuck = make_pyfuck(python_code)
+    write_file("output.py", f"exec({pyfuck})")
     print("-> output.py")
