4f0252e97e25a557c4058f615646f1de43adbe29
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-20 00:46:49 +0900

    feat: 数値をpyfuck形式に変換する関数を作成

--- pyfuck.py
+++ pyfuck.py
@@ -20,6 +20,18 @@ def write_file(filename: str, fuck: str):
     with open(filename, "w") as f:
         return f.write(fuck)
 
+# 数値をPyFuckに変換
+def num_to_fuck(num: str):
+    digit: int = len(num)  # num の文字数
+    num = int(num)
+    ones: int = int("1" * digit)  # 11111....111 の数値
+    fucks = []  # [1111, 111, 11, 1]
+    while num > 0 and ones > 0:
+        while num > 0 and ones > 0 and num >= ones:
+            num -= ones
+            fucks.append(ones)
+        ones = ones // 10
+    return "+".join(map(str, fucks))
 
 if __name__ == "__main__":
     python_code = read_file(input("FileName: "))
@@ -27,15 +39,7 @@ if __name__ == "__main__":
     pyfuck = ""
     for c in python_code:
         pyfuck += "chr("
-        ord_c = ord(c)
-        fuck = int("1" * len(str(ord_c)))
-        while ord_c != 0:
-            for i in range(int(ord_c / fuck)):
-                if pyfuck[-1] != "(":
-                    pyfuck += "+"
-                pyfuck += f"{str(fuck)}"
-            ord_c = int(ord_c % fuck)
-            fuck = int(fuck / 10)
+        pyfuck += num_to_fuck(str(ord(c)))
         pyfuck += ")+"
 
     write_file("output.py", f"exec({pyfuck[:-1]})")
